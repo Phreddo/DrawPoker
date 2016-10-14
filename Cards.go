8d6d9500d0ce39c0089016485ecb2d4a90263358
Phreddo <Phreddo@users.noreply.github.com>
2016-10-13 20:43:01 -0500

    Add files via upload

---
+++ Cards.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+//This is the file for the actual card display
+func main() {
+	fmt.Println(" +-----+\n",
+	"|A   S|\n",
+	"|     |\n",
+	"|     |\n",
+	"|     |\n",
+	"|S   A|\n",
+	"+-----+")
+}
