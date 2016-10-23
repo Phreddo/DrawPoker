ee17118cddb37b928d73bac25128705c48a8ec08
Phreddo <Phreddo@users.noreply.github.com>
2016-10-23 13:57:04 -0500

    Add files via upload

--- array.go
+++ array.go
@@ -9,6 +9,9 @@ import (
 	"math/rand"
 	"time"//I get this part as a way to seed the random shuffle function
 )
+
+var card string = "\n+-----+\n|A   S|\n|     |\n|     |\n|     |\n|S   A|\n+-----+"
+
 func Shuffle(a []string) {
 	for i := range a {
 		j := rand.Intn(i+1)
@@ -16,23 +19,15 @@ func Shuffle(a []string) {
 	}
 }
 
-/*func main() {
- 	x := [5]string{
-	"AS",
-	"KS",
-	"QS",
-	"JS",
-	"XS",
-}*/
-    
+
 func main() {
-	a := []string{//Hard-coded deck, may try to map types and values
-	"AS","KS","QS","JS","XS","9S","8S","7S","6S","5S","4S","3S","2S",
-	"AH","KH","QH","JH","XH","9H","8H","7H","6H","5H","4H","3H","2H",
-	"AC","KC","QC","JC","XC","9C","8C","7C","6C","5C","4C","3C","2C",
-	"AD","KD","QD","JD","XD","9D","8D","7D","6D","5D","4D","3D","2D",
-}
+	a := []string{//Hard-coded deck, may try to map suit types and values
+		"AS","KS","QS","JS","XS","9S","8S","7S","6S","5S","4S","3S","2S",
+		"AH","KH","QH","JH","XH","9H","8H","7H","6H","5H","4H","3H","2H",
+		"AC","KC","QC","JC","XC","9C","8C","7C","6C","5C","4C","3C","2C",
+		"AD","KD","QD","JD","XD","9D","8D","7D","6D","5D","4D","3D","2D",
+	}
 	rand.Seed(time.Now() .UnixNano())//Never would have reasoned out the UnixNano thing
 	Shuffle(a)
-	fmt.Println(a[0:5])
+	fmt.Println(a[0], a[1], a[2], a[3], a[4], card, card)
 }
